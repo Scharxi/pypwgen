12b3483653fba201f1feb1ae0e18b375210f2d5f
BufferOverflow <scharxidev@outlook.de>
2024-10-29 09:08:22 +0100

    Improve password generation and extend .gitignore
    Refactored password generation function to support disabling digits and special characters via parameters. Added `.idea` directory to `.gitignore` for IDE configuration files.

--- pypwgen.py
+++ pypwgen.py
@@ -1,22 +1,33 @@
 import string
 from random import choice
+from typing import LiteralString
+
+CHARS = string.ascii_letters
+SPECIALS = string.punctuation
+DIGITS = string.digits
+
+import typer
 
 
-chars = string.ascii_letters
-specials = string.punctuation
-digits = string.digits
-
-def gen_password(len: int = 8, *, digits=digits, specials=specials)-> str: 
+def gen_password(length: int = 8, *, digits: LiteralString | None = DIGITS,
+                 specials: LiteralString | None = SPECIALS) -> str:
+    """
+    Generates a secure password
+    :param length: The length of the password
+    :param digits: Set to None to disable digits
+    :param specials: Set to None to disable special characters
+    :return: the generated password
+    """
     password: str = ""
+    global CHARS
+    if digits is not None:
+        CHARS += digits
 
-    if digits is not None: 
-        chars + digits
-    
-    if specials is not None: 
-        chars + specials
+    if specials is not None:
+        CHARS += specials
 
-    for _ in range(len): 
-        password += choice(chars)
+    for _ in range(length):
+        password += choice(CHARS)
     return password
 
 
